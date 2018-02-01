6804783a7b407facd19bb4625daa4f3db84d6746
Adam Shire <shirea@wit.edu>
2018-02-01 15:55:28 -0500

    add time stamp

--- js/fight.js
+++ js/fight.js
@@ -1,3 +1,6 @@
+
+
+
 // create a contest. Takes two opponents as input. Returns winner and loser
 
 // Initialize Firebase
@@ -16,12 +19,13 @@ function Contest(opponent_1, opponent_2)  {
     this.opponent_2 = opponent_2;
     this.getResults = function() {
       let contestResults;
+      let timestamp = firebase.database.ServerValue.TIMESTAMP
       if (this.opponent_1 === this.opponent_2) {
          contestResults = 'tie'
        } else if (md5(this.opponent_1) > md5(this.opponent_2)) {
-         contestResults = {'winner':this.opponent_1, 'loser':this.opponent_2}
+         contestResults = {'winner':this.opponent_1, 'loser':this.opponent_2, 'timestamp':timestamp}
        } else {
-         contestResults = {'winner':this.opponent_2, 'loser':this.opponent_1}
+         contestResults = {'winner':this.opponent_2, 'loser':this.opponent_1,'timestamp':timestamp}
        }
        return contestResults;
     }
@@ -30,46 +34,42 @@ function Contest(opponent_1, opponent_2)  {
 
 
 
-//TODO: Make sure we have two opponents
-const checkOpponents = () => {
-
-}
-//TODO: listen for data changes in the database
-
-
-
 
 const fight = () => {
+  //shortcuts to DOM Elements
+  var opponent_1 = document.getElementById('opponent_1').value
+  var opponent_2 = document.getElementById('opponent_2').value
+  var resultsDiv = document.getElementById('results')
+
   //create a new contest object
-  const contest = new Contest(document.getElementById('opponent_1').value, document.getElementById('opponent_2').value)
+  const contest = new Contest(opponent_1, opponent_2)
+  const contestResults = contest.getResults();
   //make sure we have two opponents
-  if (!!(contest.opponent_1) && !!(contest.opponent_2)) {
-    let contestResults = contest.getResults(); // return a winner and loser
-    //If there's a tie don't save anything to the database. Just display a message.
-    if (contestResults === 'tie'){
-      alert("it's a tie");
-      return false;
-    }
-  console.log(contestResults);
+  if (!(contest.opponent_1) || !(contest.opponent_2)) {
+    alert("we need two opponents")
+    return false;
+  } else if (contestResults === 'tie'){
+    alert("it's a tie");
+    return false;
+  } else {
+
   // Get a database reference
   const contestsRef = firebase.database().ref('contests/');
 
   //write the contest to the database
-  let contestKey = contestsRef.push(contestResults).key;
+
+  contestsRef.push(contestResults);
 
   //get the contest from the datatabase and update UI
-  let resultsRef = firebase.database().ref('contests/').limitToLast(1);
+  let resultsRef = firebase.database().ref('contests/').orderByChild('timestamp');
   let resultsList =""
-   resultsRef.once('value', function(snapshot) {
-
+   resultsRef.once('value')
+      .then(function(snapshot) {
             snapshot.forEach (function(childSnapshot) {
-
-              resultsList += "<div> winner : " + childSnapshot.val().winner + "</div>"
-            document.getElementById('results').innerHTML = resultsList })
-          }
-     );
-
-  } else {
-    alert('we need two opponents')
-    }
+              var data = childSnapshot.val();
+              resultsList += "<div>timestamp: "+ data.timestamp + " winner: " + data.winner + "</div>"
+                          resultsDiv.innerHTML = resultsList;
+                        });
+          });
+   }
 }
